efe1f0108db220048173fd760f6772b61ee98487
majf <majf.colt@gmail.com>
2015-10-20 23:59:17 +0800

    删除无用文件 	deleted:    app/src/main/java/com/coltcn/ims/ui/fragment/StockFragment.java

---
+++ app/src/main/java/com/coltcn/ims/ui/fragment/StockFragment.java
@@ -0,0 +1,10 @@
+package com.coltcn.ims.ui.fragment;
+
+import com.coltcn.ims.ui.base.BaseFragment;
+
+/**
+ * Created by colt on 15/10/15.
+ */
+public class StockFragment extends BaseFragment {
+
+}
